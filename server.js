d2fcf61982e7ee32f36f3c64daeb4da53d778675
thai-canada <93546914+thai-canada@users.noreply.github.com>
2026-05-27 14:04:21 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,49 @@
-const PORT = 50002;
-const fs = require("fs");
-const path = require("path");
+const n_port = 50002;
+const path = require('path');
+var fs = require('fs');
+
+
+var express = require('express');
+
+
+var app = express();
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', req.headers.origin ? req.headers.origin : '*');
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+	next();
+});
+app.use(express.json());
+
+
+app.use(express.static(__dirname));
+
+app.get('/', function(req, res) {
+	//res.sendFile(path.join('index.html'));
+	res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/ver',(req,res)=>{
+	res.json({"version": '1.0.0'});
+})
+
 
 const dataDir = path.join(__dirname, "data");
 const dbFile = path.join(dataDir, "db.json");
 if (!fs.existsSync(dataDir)) fs.mkdirSync(dataDir);
 if (!fs.existsSync(dbFile)) fs.writeFileSync(dbFile, '{"info": []}', "utf8");
 
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+var router = jsonServer.router(path.join('data/db.json'));
+const middlewares = jsonServer.defaults();
+server.use(middlewares);
+server.use(router);
 
-const express = require('express');
-const jsonServer = require("json-server");
-const { strictEqual } = require("assert/strict");
-const app = express();
-const router = jsonServer.router(path.join(__dirname, "data/db.json"));
-const middlewares = jsonServer.defaults({ logger: false });
-const db = router.db;
-
-
-// Define routes BEFORE middlewares to take precedence over static file serving
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
-app.get("/index.html", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
-
-// Serve static files from the root directory and 'js' subfolder using express.static
-app.use(express.static(__dirname));
-// app.use('/js', express.static(path.join(__dirname, 'js')));
-
-app.use(router);
-app.listen(PORT, () => {
-    const text = `JSON server is running on port: ${PORT}`;
-    console.log(text);
-});
-
-
-// http://localhost:50002/logs?type=button
+// server.listen(n_port, () => {
+// 	console.log('JSON Server is running at: ' + n_port.toString());
+// })
+app.use('/json', server.use(router));
+app.listen(n_port, () => {
+    console.log('JSON Server is running at: ' + n_port.toString());
+})
